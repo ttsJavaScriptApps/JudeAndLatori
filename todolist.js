0b19777320303c9676180a86bec43c64b939cb29
Jude Harding <jharding@genesisgroupusa.com>
2016-02-12 17:16:45 -0500

    started the todo list

---
+++ todolist.js
@@ -0,0 +1,54 @@
+
+var q = document.querySelector.bind(document);
+var qa = document.querySelectorAll.bind(document);
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+// <button type="button" onclick="modifyColor()">Click Me</button>
+//     <script>
+//     function modifyColor() {
+//         document.getElementById("example").style.color = "aqua";
+//         document.getElementById("example").style.backgroundColor = "gray";
+//     }
+//     </script>
+// ----
+//     The setItem() method will add (or update) a key/value pair to the Storage object
+// ----
+// HTML
+//
+// <input id="username"></input>
+// JS
+//
+// //Get a single node
+// var el = document.getElementById('username');
+// ----
+// var user = {
+//   name : "Shane",
+//   job : "instructor"
+// };
+//
+// var storeUser = JSON.stringify(user);
+//
+// localStorage.setItem('userInfo', storeUser);
+//
+// localStorage.getItem('userInfo');
+// // "{"name":"Shane","job":"instructor"}"
+//
+// var parsedData = localStorage.getItem('userInfo');
+//
+// console.log(parsedData);
+// // {"name":"Shane","job":"instructor"}
+// ----
+//
+//
